a505bcd5d167a1bcd4bfe56f4cc5304d424e2733
Jonas Hecht <jonas.hecht@codecentric.de>
2019-04-12 11:46:38 +0200

    Trying to support Mac in tests - but at the end found that Docker Native doesn't run on Virtualized Mac because of https://github.com/jonashackt/vagrant-macosx/issues/1 :(

--- docker/molecule/tests/test_docker.py
+++ docker/molecule/tests/test_docker.py
@@ -5,17 +5,19 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+def test_run_hello_world_container_successfully_on_macos(host, Command):
+
+    if host.system_info.type == "darwin":
+        hello_world_ran = Command("sudo docker run hello-world")
+        assert 'Hello from Docker!' in hello_world_ran.stdout
+
+    assert host.system_info.distribution == "Mac OS X"
+
 
 def test_is_docker_installed(host):
     package_docker = host.package('docker-ce')
 
     assert package_docker.is_installed
-
-
-def test_vagrant_user_is_part_of_group_docker(host):
-    user_vagrant = host.user('vagrant')
-
-    assert 'docker' in user_vagrant.groups
 
 
 def test_run_hello_world_container_successfully(host):
